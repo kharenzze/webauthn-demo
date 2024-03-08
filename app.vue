15105468b5a3914ba8e6aff0cbdacc5b44d8f163
Paolo Ráez <str237@gmail.com>
2024-03-08 21:20:23 +0000

    WIP registering

--- app.vue
+++ app.vue
@@ -1,5 +1,38 @@
+<script setup lang="ts">
+import { client } from '@passwordless-id/webauthn'
+
+const challenge = useState('challenge', () => crypto.randomUUID())
+const name = ref('Paolo')
+
+const onSubmit = async () => {
+  try {
+    const registration = await client.register(name.value, challenge.value, {
+      authenticatorType: "auto",
+      userVerification: "required",
+      timeout: 60000,
+      attestation: true,
+      userHandle: "recommended to set it to a random 64 bytes value",
+      debug: true
+    })
+    $fetch('/api/passkey/register', {
+      method: 'POST',
+      body: registration
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    Hi there!
   </div>
+  <p>Challenge</p>
+  <p>{{ challenge }}</p>
+  <input type="text" v-model="name">
+  <button @click="onSubmit">
+    Sign in
+  </button>
 </template>
