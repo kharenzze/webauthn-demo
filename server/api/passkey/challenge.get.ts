4f6a93a7fd05ec5a4346aa4628d4198b7491fd58
Paolo Ráez <str237@gmail.com>
2024-03-10 16:12:59 +0000

    Await

--- server/api/passkey/challenge.get.ts
+++ server/api/passkey/challenge.get.ts
@@ -3,7 +3,7 @@ import { KeyGen } from "~/src/storage";
 export default defineEventHandler(async (event) => {
   const challenge = crypto.randomUUID();
   const key = KeyGen.challenge(challenge);
-  event.context.cloudflare.env.KV.put(key, "true", {
+  await event.context.cloudflare.env.KV.put(key, "true", {
     expirationTtl: 60, //seconds
   });
   return { challenge };
