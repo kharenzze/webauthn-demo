c14936d56bc6c8e9399c376d17059c19f8a5283c
Paolo Ráez <str237@gmail.com>
2024-03-10 10:34:37 +0000

    Refactor

--- composables/useJWTSession.ts
+++ composables/useJWTSession.ts
@@ -7,10 +7,9 @@ export const useJWTSession = async () => {
     const jwt = useCookie("x-auth");
     const evt = useRequestEvent();
     await callOnce(async () => {
-      if (!jwt.value || !evt) {
-        return undefined;
+      if (jwt.value && evt) {
+        session.value = await verifyJWT(evt)(jwt.value);
       }
-      session.value = await verifyJWT(evt)(jwt.value);
     });
   }
   return session;
