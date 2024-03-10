b011eb60cabd362c760e6f038a7cee331766f147
Paolo Ráez <str237@gmail.com>
2024-03-10 10:39:55 +0000

    Install nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   nitro: {
-    preset: "cloudflare-pages"
+    preset: "cloudflare-pages",
   },
 
-  modules: ["nitro-cloudflare-dev"]
-})
+  modules: ["nitro-cloudflare-dev", "@nuxt/ui"],
+});
